e7cef992cf0f89c0f6b7cbe2fadacbfd160e03c7
Chloe Stanton <cstanton5@ewu.edu>
2025-10-08 09:44:41 -0700

    Refactored BaseLoggerMixins.cs to use helper method.

--- Logger/BaseLoggerMixins.cs
+++ Logger/BaseLoggerMixins.cs
@@ -5,32 +5,27 @@ namespace Logger;
 
 public static class BaseLoggerMixins
 {
-    public static void Error(this BaseLogger logger, string message, params object[] args)
+    public static void LogFormatHelper(this BaseLogger logger, LogLevel level, string message, params object[] args)
     {
         ArgumentNullException.ThrowIfNull(logger);
-
         string formattedMessage = string.Format(CultureInfo.InvariantCulture, message, args);
-        logger.Log(LogLevel.Error, formattedMessage);
+        logger.Log(level, formattedMessage);
+    }
+
+    public static void Error(this BaseLogger logger, string message, params object[] args)
+    {
+        logger.LogFormatHelper(LogLevel.Error, message, args);
     }
     public static void Warning(this BaseLogger logger, string message, params object[] args)
     {
-        ArgumentNullException.ThrowIfNull(logger);
-
-        string formattedMessage = string.Format(CultureInfo.InvariantCulture, message, args);
-        logger.Log(LogLevel.Warning, formattedMessage);
+        logger.LogFormatHelper(LogLevel.Warning, message, args);
     }
     public static void Information(this BaseLogger logger, string message, params object[] args)
     {
-        ArgumentNullException.ThrowIfNull(logger);
-
-        string formattedMessage = string.Format(CultureInfo.InvariantCulture, message, args);
-        logger.Log(LogLevel.Information, formattedMessage);
+        logger.LogFormatHelper(LogLevel.Information, message, args);
     }
     public static void Debug(this BaseLogger logger, string message, params object[] args)
     {
-        ArgumentNullException.ThrowIfNull(logger);
-
-        string formattedMessage = string.Format(CultureInfo.InvariantCulture, message, args);
-        logger.Log(LogLevel.Debug, formattedMessage);
+        logger.LogFormatHelper(LogLevel.Debug, message, args);
     }
 }

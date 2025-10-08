a5d80a96164e1dc5c784234cf9fd6ca95e87c994
Chloe Stanton <156254816+c-stanton@users.noreply.github.com>
2025-10-07 21:31:14 -0700

    Update FileLogger.cs
    Removed unnecessary comment.

--- Logger/FileLogger.cs
+++ Logger/FileLogger.cs
@@ -19,7 +19,7 @@ namespace Logger
 
         public override void Log(LogLevel logLevel, string message)
         {
-            string timeStamp = DateTime.Now.ToString("MM/dd/yyyy hh:mm:ss tt", CultureInfo.InvariantCulture); //COME BACK TO THIS LINE WITH A BETTER UNDERSTANDING OF ClassName
+            string timeStamp = DateTime.Now.ToString("MM/dd/yyyy hh:mm:ss tt", CultureInfo.InvariantCulture);
             string logText = $"{timeStamp} {ClassName} {logLevel}: {message}";
             File.AppendAllText(_filePath, logText + Environment.NewLine);
         }
